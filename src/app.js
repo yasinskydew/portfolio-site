99d584ac4fc6f013e3888765a5e108ff8ac7f66c
Vladzimir Yasinsky <yasinsky2015@gmail.com>
2019-06-03 07:51:41 +0300

    first-commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+app = express()
+
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) =>{
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me'
+    })
+})
+
+app.get('/contacts', (req, res) => {
+    res.render('contacts')
+})
+
+
+app.get('/works', (req, res) => {
+    res.render('works')
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found.'
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
+})
